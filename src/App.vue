f457f8c1d4455d2739f94680075f1eecd72a7aad
AbigailCui <1250682372@qq.com>
2021-09-13 02:24:44 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <Layout />
+    <Player />
+    <MiniPlayer />
+    <Playlist />
+    <ShareReader />
+  </div>
+</template>
+
+<script>
+import Layout from "@/layout"
+import MiniPlayer from "@/components/mini-player"
+import Playlist from "@/components/playlist"
+import Player from "@/components/player"
+import ShareReader from "@/components/share-reader"
+
+export default {
+  metaInfo() {
+    return {
+      title: "欢迎来到sshPlayer"
+    }
+  },
+  components: { Layout, MiniPlayer, Playlist, Player, ShareReader }
+}
+</script>
+
+<style lang="scss">
+#app {
+  height: 100%;
+  background-color: var(--body-bgcolor);
+  font-size: $font-size;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,42 @@
+const WorkboxPlugin = require("workbox-webpack-plugin");
+
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  outputDir: 'music',
+  configureWebpack: {
+    devtool: isProd ? false: 'source-map',
+    devServer: {
+      open: true,
+      proxy: {
+        '/netease-api': {
+          target: 'http://localhost:3000',
+          pathRewrite: { '^/netease-api': '' },
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+      host: '0.0.0.0',
+      port: 8888,
+    },
+    externals: isProd ? {
+      vue: 'Vue',
+      'vue-router': 'VueRouter',
+      vuex: 'Vuex',
+      axios: 'axios',
+    }: {},
+    plugins: [
+      new WorkboxPlugin.GenerateSW()
+    ]
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        data: `
+          @import "~@/style/variables.scss";
+          @import "~@/style/mixin.scss";
+        `,
+      },
+    },
+  },
+}
